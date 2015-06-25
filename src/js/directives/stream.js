591f9d7677d84ddcec943c888406323fc63d3931
Labalou <louis.audeon@mail.be>
2015-06-25 21:29:07 +0200

    stream picam js code to angular directive

--- src/js/directives/stream.js
+++ src/js/directives/stream.js
@@ -6,22 +6,19 @@
   */
  angular
  	.module('kitcat')
- 		.directive('stream', function ($scope){
+ 		.directive('stream', function (){
 		    return {
 		    	restrict: 'A',
 		    	scope: true,
 		    	link: function (scope, element, attr) 
 		    	{
-					element.on('ready', function(event) {
-
-						// Show loading notice
-						var canvas = element;
+						var canvas = element[0];
 						var ctx = canvas.getContext('2d');
 						ctx.fillStyle = '#444';
 						ctx.fillText('Loading...', canvas.width/2-30, canvas.height/3);
 
-				 		// Setup the WebSocket connection and start the player
 						var client = new WebSocket('ws://'+scope.user.raspIp+':'+scope.user.raspPortCam+'/');
 						var player = new jsmpeg(client, {canvas:canvas});
-
-					});
+				}
+			}
+		});
